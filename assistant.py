aea2f84941c4394a82e7b19dd3eafca82d1dc9b5
TypeA2 <nuanlim29@gmail.com>
2018-06-11 16:23:59 +0200

    env test

--- assistant.py
+++ assistant.py
@@ -1,17 +1,3 @@
-import discord
-from discord.ext import commands
+import os
 
-bot = commands.Bot(command_prefix='$')
-
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
-
-@bot.command()
-async def greet(ctx):
-    await ctx.send(":smiley: :wave: Hello, there!")
-
-bot.run("token")
+print(os.environ["DISCORD_TOKEN"])
